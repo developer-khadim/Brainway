61bf795318bcab2124c58a31d0e6d39419c8d3b8
Your Name <you@example.com>
2024-07-16 23:56:14 +0500

    add

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from './Heading';
+
+const Benefits = ({className,title,text,teg}) => {
+  return (
+    <section id='features' >
+        <div >
+        
+        </div>
+        
+    </section>
+  );
+}
+
+export default Benefits
