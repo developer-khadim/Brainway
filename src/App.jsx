a7249d0cf66ec5b132e31c24387df98f6242970f
Your Name <you@example.com>
2024-07-18 01:18:22 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from './components/Benefits';
 import Callabration from './components/Callabration';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Services from './components/Services';
 const App = () => {
   return (
     < >
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
        <Benefits/>
        <Callabration/>
+       <Services/>
       </div>
       <ButtonGradient/>
     </>
--- src/components/Callabration.jsx
+++ src/components/Callabration.jsx
@@ -12,15 +12,15 @@ const Callabration = () => {
           <h2 className="h2 mb-4 md:mb-8">
             Ai Chat App for seamless collaboration
           </h2>
-          <ul className="max-w-[22rem] mb-10 md:mb-14 ">
+          <ul className="max-w-[22rem] mb-10 md:mb-14">
             {collabContent.map((item) => (
               <li key={item.id} className="mb-3 py-3">
-                <div className="flex item-center">
-                  <img src={check} alt={item.title} width={24} height={24} />{" "}
+                <div className="flex items-center">
+                  <img src={check} alt={item.title} width={24} height={24} />
                   <h6 className="body-2 ml-5">{item.title}</h6>
                 </div>
                 {item.text && (
-                  <p className="body-2 mt-3 text-n-4"> {item.text} </p>
+                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
                 )}
               </li>
             ))}
@@ -32,16 +32,45 @@ const Callabration = () => {
             {collabText}
           </p>
 
-          <div className="relative left-1/2 flex w-[22rem] 
-          aspect-square border border-n-6 rounded-full 
-          -translate-x-1/2 scale-75 md:scale-100 " >
-            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full" >
-                <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-" >
-                    <div>
-                        <img src={brainwaveSymbol} alt="brainway" width={48} height={48} />
-                    </div>
+          <div
+            className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale-75 md:scale-100"
+          >
+            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-">
+                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                  <img
+                    src={brainwaveSymbol}
+                    alt="brainway"
+                    width={48}
+                    height={48}
+                  />
                 </div>
+              </div>
             </div>
+            <ul>
+              {collabApps.map((app, index) => (
+              <li 
+              key={app.id} 
+              className="absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom"
+              style={{ transform: `rotate(${index * 46}deg)` }}
+            >
+              <div 
+                className="relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl"
+                style={{ transform: `rotate(-${index * 46}deg)` }}
+              >
+                <img 
+                  className="m-auto" 
+                  width={app.width} 
+                  height={app.height} 
+                  src={app.icon} 
+                  alt={app.title} 
+                />
+              </div>
+            </li>
+              ))}
+            </ul>
+            <LeftCurve/>
+            <RightCurve/>
           </div>
         </div>
       </div>
